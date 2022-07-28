3afb5bbb9bca2dde70951bac70da3186515b650b
doguhan önder <doguhan@doguhan-MacBook-Pro.local>
2022-07-28 11:06:59 +0300

    build edildi

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -29,7 +29,7 @@ const Register = () => {
        
       <input className=' shadow-sm focus:ring-indigo-500 block bg-black focus:border-indigo-500 sm:text-sm border-gray-500 rounded-full text-white' type="text" placeholder="e posta" value={email} onChange={e => setEmail(e.target.value)} />
       <input className=' shadow-sm focus:ring-indigo-500 block bg-black focus:border-indigo-500 sm:text-sm border-gray-500 rounded-full text-white'  type="password" placeholder="password" value={password} onChange={e => setPassword(e.target.value)} />
-      <button type="submit" disabled={!email || !password} className=' cursor-pointer inline-flex item-center px-4 py-2 border border-transparent text-white mt-2 bg-zinc-500 rounded-full hover:bg-neutral-800'>kayıt ol</button>
+      <button type="submit" disabled={!email || !password} className=' cursor-pointer inline-flex item-center px-4 py-2 border border-transparent text-white mt-2 bg-zinc-500 rounded-full hover:bg-neutral-800'>kayıt Ol</button>
       </form>
     </div>
   )
